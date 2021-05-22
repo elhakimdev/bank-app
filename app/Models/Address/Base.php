3aca90eaf0bb619869ad84fa0d028eb1e97012d8
elhakimdev <abdulelhakim68@gmail.com>
2021-05-22 14:15:24 +0700

    hot fix search filter scope

--- app/Models/Address/Base.php
+++ app/Models/Address/Base.php
@@ -9,16 +9,16 @@ class Base extends Model
 {
     public $timestamps = false;
     protected $keyType = 'string';
-    protected $searchableColumns = ['code', 'name'];
+    // protected $searchableColumns = ['code', 'name'];
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
         $this->table = $this->table;
     }
-    public function scopeSearch(object $query, string $keyword)
+    public function scopeSearch($query, string $keyword)
     {
-        if ($keyword && $this->searchableColumns) {
-            $query->whereLike($this->searchableColumns, $keyword);
+        if ($keyword) {
+            $query->where('name', 'like', $keyword . "%");
         }
     }
 }
--- routes/area.php
+++ routes/area.php
@@ -1,12 +1,24 @@
 <?php
 
+use App\Models\Address\Province;
 use Illuminate\Support\Facades\Route;
 use App\Services\Administrative\IndonesiaService;
+use Illuminate\Http\Request;
 
 Route::prefix('indonesia')->group(function () {
        Route::prefix('provinces')->group(function () {
+              // return $request->fullUrl();
+              Route::get('/search', function (Request $request) {
+                     $qs = $request->fullUrl();
+                     $qs = explode('?', $qs);
+                     $qs = $qs[1];
+                     $param = explode('=', $qs);
+                     $indonesia = new IndonesiaService();
+                     return $indonesia->search($param[1])->allProvinces();
+                     // return $param[1];
+              });
               Route::get('/', function (IndonesiaService $indonesia) {
-                     return $indonesia->allProvinces();
+                     return $indonesia->paginateProvinces();
               });
               Route::get('/{province}', function ($province, IndonesiaService $indonesia) {
                      return $indonesia->findProvince($province, ['cities', 'districts.villages']);
@@ -14,7 +26,7 @@ Route::prefix('indonesia')->group(function () {
        });
        Route::prefix('cities')->group(function () {
               Route::get('/', function (IndonesiaService $indonesia) {
-                     return $indonesia->allCities();
+                     return $indonesia->paginateCities();
               });
               Route::get('/{city}', function ($city, IndonesiaService $indonesia) {
                      return $indonesia->findCity($city, ['districts.villages']);
@@ -22,7 +34,7 @@ Route::prefix('indonesia')->group(function () {
        });
        Route::prefix('districts')->group(function () {
               Route::get('/', function (IndonesiaService $indonesia) {
-                     return $indonesia->allDistricts();
+                     return $indonesia->paginateDistricts();
               });
               Route::get('/{district}', function ($ditrict, IndonesiaService $indonesia) {
                      return $indonesia->findDistrict($ditrict, ['villages']);
@@ -30,7 +42,7 @@ Route::prefix('indonesia')->group(function () {
        });
        Route::prefix('villages')->group(function () {
               Route::get('/', function (IndonesiaService $indonesia) {
-                     return $indonesia->allVillages();
+                     return $indonesia->paginateVillages();
               });
               Route::get('/{village}', function ($village, IndonesiaService $indonesia) {
                      return $indonesia->findVillage($village, []);
