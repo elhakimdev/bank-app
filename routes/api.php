9c0100d8292f93c65d6b53469090ada2f0cf77a4
elhakimdev <abdulelhakim68@gmail.com>
2021-05-27 13:32:14 +0700

    fix

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,11 @@ use App\Http\Controllers\API\Actions\Roles\RoleActionController;
 use App\Http\Controllers\API\Actions\Permissions\PermissionActionController;
 use App\Http\Controllers\Api\Resources\ProfileController;
 use App\Http\Controllers\AuthTokenController;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-});
+})->middleware('auth:sanctum');
 /**
  * -----------------------------------------------------------------------------
  * Routes For Authentication Using Token / Bearer 
